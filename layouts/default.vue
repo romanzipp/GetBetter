c1837af6c2ac7ea8f15542c8709bca2f7072b226
Roman Zipp <ich@ich.wtf>
2021-09-01 12:20:22 +0200

    Add title

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,28 @@
 
     <div>
 
+        <div class="h-screen flex">
+
+            <div class="w-1/3 flex items-center bg-green-100">
+
+                <div class="font-heading-secondary font-normal text-5xl">
+                    Get Better Blog Title
+                </div>
+
+                <div class="font-heading-primary font-normal text-5xl">
+                    Get Better Blog Title
+                </div>
+
+                <img src="/get_better_logo_transparent.png"
+                     class="transform translate-x-1/2"
+                     alt="">
+
+            </div>
+
+            <div class="w-2/3 bg-white" />
+
+        </div>
+
         <div class="w-full p-6">
 
             <div class="container mx-auto">
