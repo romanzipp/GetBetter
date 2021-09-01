c3090fb2342b3ed36727395799d91c9c4ec6c3f5
Roman Zipp <ich@ich.wtf>
2021-09-01 13:35:42 +0200

    b66d331

---
+++ _nuxt/ce8c7b1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{302:function(n,e,t){"use strict";t.r(e);var l=t(46),component=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
