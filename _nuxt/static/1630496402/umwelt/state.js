1bb7b4dbbb68aa6818ae09a6101e3c696e6c4a01
romanzipp <romanzipp@users.noreply.github.com>
2021-09-01 11:40:04 +0000

    deploy: f740aa46bd117ef531652748cedf68785fde0f4d

---
+++ _nuxt/static/1630496402/umwelt/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630496402",layout:"default",error:a,serverRendered:true,routePath:"\u002Fumwelt",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"aaf890f2"}}}}(null));
