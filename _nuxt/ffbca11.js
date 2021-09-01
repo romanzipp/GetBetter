060f1477934091fcb1fc9585b7c39afc86701e26
romanzipp <romanzipp@users.noreply.github.com>
2021-09-01 15:27:00 +0000

    deploy: e66624e593b0ba04999ff2034d01737e30217e96

--- _nuxt/ffbca11.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{310:function(t,e,n){"use strict";n.r(e);var r={head:function(){return{title:"Get Better"}}},l=n(14),component=Object(l.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/static/1630510018/ernährung/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630510018",layout:"content",error:a,serverRendered:true,routePath:"\u002Fern%C3%A4hrung",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d74ce817"}}}}(null));
