e60bebe0d1f0fd432ab434a8ab5ddfaf077ef8fd
Roman Zipp <ich@ich.wtf>
2021-09-01 12:18:33 +0200

    Add fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ export default {
 
     // Global CSS: https://go.nuxtjs.dev/config-css
     css: [
+        '~/assets/css/fonts.css',
     ],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,46 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: {
+        content: [
+            'content/**/**.md',
+            'pages/**/*.vue',
+            'layouts/**/*.vue',
+            'components/**/*.vue',
+        ],
+    },
+    darkMode: false,
+    theme: {
+        fontFamily: {
+            'heading-primary': [
+                'Kazimir',
+                'serif',
+            ],
+            'heading-secondary': [
+                '"Brandon Grotesque"',
+                'sans-serif',
+            ],
+            body: [
+                'Kazimir',
+            ],
+        },
+        extend: {
+            colors: {
+                green: {
+                    50: '#f6faf5',
+                    100: '#edf5eb',
+                    200: '#d2e7cc',
+                    300: '#b7d9ad',
+                    400: '#81bc70',
+                    500: '#4b9f32',
+                    600: '#448f2d',
+                    700: '#387726',
+                    800: '#2d5f1e',
+                    900: '#254e19',
+                },
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
