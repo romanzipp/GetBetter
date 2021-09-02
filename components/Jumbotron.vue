0af825ade45246865872d7ee5d830d04f1ba148c
aaamat <89915466+aaamat@users.noreply.github.com>
2021-09-02 16:56:42 +0200

    Update Jumbotron.vue

--- components/Jumbotron.vue
+++ components/Jumbotron.vue
@@ -9,7 +9,7 @@
             </div>
 
             <p class="mt-12 font-serif text-xl leading-normal text-left md:text-center">
-                Gerade in jüngster Zeit bekommen wir alle die Auswirkungen der Klimakrise zu spüren. Das die anthropogenen Einflüsse die Hauptursache der globalen Klimaentwicklung sind, ist mittlerweile indisputabel. Wir sollten uns allen bewusst sein, dass wir jetzt Änderungen brauchen. Dieses Projekt soll interessierten Menschen informieren und den Umstieg auf eine vegane Ernährung erleichtern.
+                Gerade in jüngster Zeit bekommen wir alle die Auswirkungen der Klimakrise zu spüren. Das die anthropogenen Einflüsse die Hauptursache der globalen Klimaentwicklung sind, ist mittlerweile indisputabel. Wir sollten uns allen bewusst sein, dass wir jetzt Änderungen brauchen. Dieses Projekt soll Menschen informieren und den Umstieg auf eine vegane Ernährung erleichtern.
             </p>
 
         </div>
