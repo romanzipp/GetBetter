5a3187c5ebc482300aee35052b98d8b1b411b3e5
romanzipp <romanzipp@users.noreply.github.com>
2021-09-02 17:58:12 +0000

    deploy: 99fb08bb3d04f1166a2b77a6b4ce4c171769a899

--- _nuxt/static/1630605392/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630605392",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ee1893ca"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1630605490/ethik/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630605490",layout:"content",error:a,serverRendered:true,routePath:"\u002Fethik",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b87efa92"}}}}(null));
