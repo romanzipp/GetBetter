19ef23b01710ed832e2e3e1f80fd3c835bf93d7f
romanzipp <romanzipp@users.noreply.github.com>
2021-09-02 17:44:03 +0000

    deploy: b8a596d90e1b93d1f129ba89919c6d04a5d7c6de

---
+++ _nuxt/static/1630604641/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630604641",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ec5fbeae"}}}}(null,"\u002F"));
