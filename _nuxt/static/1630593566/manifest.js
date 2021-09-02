8ab6f0ec1f0c78155b7c2be376f4fbbf7de88104
aaamat <aaamat@users.noreply.github.com>
2021-09-02 14:45:44 +0000

    deploy: 17a7a6fc9a5e93e09b8e246d2b666e8f3162962a

--- _nuxt/static/1630593566/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fernährung","\u002F","\u002Ffaq","\u002Fethik","\u002Fumwelt"]})
---
+++ _nuxt/static/1630593942/faq/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630593942",layout:"content",error:a,serverRendered:true,routePath:"\u002Ffaq",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d5719648"}}}}(null));
