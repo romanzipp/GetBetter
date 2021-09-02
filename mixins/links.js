cf161966d82e0dd6910e4d8d13ac9417ab7e747d
aaamat <89915466+aaamat@users.noreply.github.com>
2021-09-02 19:37:48 +0200

    Update links.js

--- mixins/links.js
+++ mixins/links.js
@@ -17,7 +17,7 @@ export default {
                 title: 'Ethik',
                 url: '/ethik',
                 image: '/undraw_wandering_mind.svg',
-                intro: 'Unsere Gesellschaft und die Wissenschaft entwickeln sich stehts weiter, doch bei der ethischen Frage ob wir Lebewesen für unseren Genuss quälen und töten dürfen, scheint es nur ganz langsam Fortschritt zu geben. Wir haben täglich die Wahl: Kaufen wir tierische Produkte und verantworten das qualvolle Leben und den Tod von Lebewesen, die nicht sterben wollen, oder stehen wir für die Lebewesen ein, die selbst keine Stimme haben sich zu äußern und leben ohne Tierleid zu verursachen – vegan.',
+                intro: 'Unsere Gesellschaft und die Wissenschaft entwickeln sich stets weiter, doch bei der ethischen Frage ob wir Lebewesen für unseren Genuss quälen und töten dürfen, scheint es nur ganz langsam Fortschritt zu geben. Wir haben täglich die Wahl: Kaufen wir tierische Produkte und verantworten das qualvolle Leben und den Tod von Lebewesen, die nicht sterben wollen, oder stehen wir für die Lebewesen ein, die selbst keine Stimme haben sich zu äußern und leben ohne Tierleid zu verursachen – vegan.',
             },
         ],
     }),
