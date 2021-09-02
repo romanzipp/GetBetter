acffd9e4f1aac4a2c1b647463e4a73d4ebf361f2
aaamat <aaamat@users.noreply.github.com>
2021-09-02 20:21:32 +0000

    deploy: 1f01afc1cb98d759daa1d033ed7bdc4171f86956

---
+++ _nuxt/static/1630614091/umwelt/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630614091",layout:"content",error:a,serverRendered:true,routePath:"\u002Fumwelt",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b1e8db7a"}}}}(null));
