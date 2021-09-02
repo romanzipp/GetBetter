4673db0ce29056b84bc2d356df49aa48c996108d
romanzipp <romanzipp@users.noreply.github.com>
2021-09-02 22:01:25 +0000

    deploy: d20c50c0395d2421364265670804ca1fce7f12b2

---
+++ _nuxt/static/1630620082/ernaehrung/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630620082",layout:"content",error:a,state:{dark:false},serverRendered:true,routePath:"\u002Fernaehrung",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"98e1a0d4"}}}}(null));
