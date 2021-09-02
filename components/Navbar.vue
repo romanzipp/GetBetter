e8ac654d15bcd555aadbd8d271204bd626a3f81f
Roman Zipp <ich@ich.wtf>
2021-09-02 23:50:57 +0200

    Update navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -8,7 +8,7 @@
             <router-link v-for="link in links"
                          :key="link.url"
                          :to="link.url"
-                         class="px-4 py-2 tracking-wide text-xl font-sans leading-normal hover:text-green-700 dark:hover:text-greener-100">
+                         class="px-4 py-2 tracking-wide text-base lg:text-xl font-sans leading-normal hover:text-green-700 dark:hover:text-greener-100">
                 {{ link.title }}
             </router-link>
 
@@ -29,17 +29,17 @@
         <div class="order-3 lg:order-3 flex-1 flex items-center justify-center lg:justify-start space-x-0 lg:space-x-4">
 
             <router-link to="/"
-                         class="px-4 py-2 tracking-wide text-xl font-sans leading-normal hover:text-green-700 dark:hover:text-greener-100">
+                         class="px-4 py-2 tracking-wide text-base lg:text-xl font-sans leading-normal hover:text-green-700 dark:hover:text-greener-100">
                 Startseite
             </router-link>
 
             <router-link to="/projekt"
-                         class="px-4 py-2 tracking-wide text-xl font-sans leading-normal hover:text-green-700 dark:hover:text-greener-100">
+                         class="px-4 py-2 tracking-wide text-base lg:text-xl font-sans leading-normal hover:text-green-700 dark:hover:text-greener-100">
                 Über uns
             </router-link>
 
             <router-link to="/faq"
-                         class="px-4 py-2 tracking-wide text-xl font-sans leading-normal hover:text-green-700 dark:hover:text-greener-100">
+                         class="px-4 py-2 tracking-wide text-base lg:text-xl font-sans leading-normal hover:text-green-700 dark:hover:text-greener-100">
                 FAQ
             </router-link>
 
