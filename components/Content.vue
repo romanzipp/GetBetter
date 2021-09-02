948ab7790203ecdac35e06a896f77b0800fb9d16
Roman Zipp <ich@ich.wtf>
2021-09-02 18:26:10 +0200

    Add sticky nav

--- components/Content.vue
+++ components/Content.vue
@@ -8,14 +8,16 @@
 
         <div v-if="showNav && anchors.length > 0"
              @click="showNav = false"
-             class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-25" />
+             class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-25 z-10" />
 
         <div v-if="anchors.length > 0"
              :class="{
                  'left-1/2 z-10 -translate-x-1/2 -translate-y-1/2': showNav,
-                 'hidden xl:block pr-4 transform -translate-x-full': !showNav
+                 'hidden xl:block pr-4 transform -translate-x-full': !showNav,
+                 'fixed xl:top-10': stick,
+                 'fixed xl:absolute': !stick,
              }"
-             class="fixed">
+             class="h-full z-20">
 
             <div class="p-4 mr-0 xl:mr-4 w-64 bg-green-200">
 
@@ -46,12 +48,13 @@
 
         </div>
 
-        <div class="prose prose-green max-w-none mx-auto overflow-x-auto">
+        <div ref="sticky-guard"
+             class="prose prose-green max-w-none mx-auto overflow-x-auto">
             <nuxt-content ref="content"
                           :document="document" />
         </div>
 
-        <div class="block xl:hidden fixed mr-10 mb-10 bottom-0 right-0">
+        <div class="block xl:hidden fixed mr-10 mb-10 bottom-0 right-0 z-20">
             <button @click="showNav = !showNav"
                     class="rounded-full bg-green-300 focus:bg-green-400 p-4 shadow">
                 Nav
@@ -76,10 +79,10 @@ export default {
         anchors: [],
         activeAnchor: null,
         showNav: false,
+        stick: false,
     }),
 
     mounted() {
-        console.log(this.document.title);
         for (const el of this.document.body.children) {
             if (!el.tag || !el.children) {
                 continue;
@@ -102,6 +105,18 @@ export default {
                 depth: parseInt(el.tag[1], 10),
             });
         }
+
+        window.addEventListener('scroll', () => {
+            const rect = this.$refs['sticky-guard'].getBoundingClientRect();
+
+            this.stick = rect.y <= 0;
+            console.log(rect.y, this.stick);
+            // if (rect.y <= 10) {
+            //    const offset = Math.min(rect.y, 0) * -1;
+            //    console.log(offset);
+            //    this.$refs['sticky-container'].classList
+            // }
+        });
     },
 
     methods: {
