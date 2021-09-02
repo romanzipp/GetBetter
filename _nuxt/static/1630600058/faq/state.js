2b5c71afbdbb96557e2d68141ae53afdcc00307a
romanzipp <romanzipp@users.noreply.github.com>
2021-09-02 16:28:31 +0000

    deploy: ce26ee5ad81b67083c15181441f0a13b88c0292f

--- _nuxt/static/1630600058/faq/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630600058",layout:"content",error:a,serverRendered:true,routePath:"\u002Ffaq",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"39b52a39"}}}}(null));
--- _nuxt/static/1630600058/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630600058",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"39b52a39"}}}}(null,"\u002F"));
