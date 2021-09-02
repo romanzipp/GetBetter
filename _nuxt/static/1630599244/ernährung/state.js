5f3a245c216f66bfbb7ecd74214e1f9130efb29d
aaamat <aaamat@users.noreply.github.com>
2021-09-02 16:14:06 +0000

    deploy: b30bc218d8382fca5c2ee018115e39381dca0f52

---
+++ _nuxt/static/1630599244/ernährung/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630599244",layout:"content",error:a,serverRendered:true,routePath:"\u002Fern%C3%A4hrung",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f0cffde5"}}}}(null));
