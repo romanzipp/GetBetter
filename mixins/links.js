a8d5aeb0d31846b493487835c47115d0fd20c957
aaamat <89915466+aaamat@users.noreply.github.com>
2021-09-02 16:47:06 +0200

    Update links.js

--- mixins/links.js
+++ mixins/links.js
@@ -5,19 +5,21 @@ export default {
                 title: 'Umwelt',
                 url: '/umwelt',
                 image: '/undraw_Nature_fun_re.svg',
-                intro: 'Aber was können wir tun? Ist die vegane Ernährung die Lösung aller Probleme? Wieviel Einfluss hat die Produktion tierischer Produkte auf die Klimaentwicklung?',
+                intro: 'Unser Planet ist unser Lebensraum, für alle jetzigen und für alle kommenden Generationen. Unser oberstes Ziel sollte es sein, diesen Lebensraum zu bewahren. »Eine vegane Ernährung ist wahrscheinlich die effektivste Weise den eigenen ökologischen Fußabdruck zu reduzieren und das nicht nur in Bezug auf Treibhausgase sondern auch auf die weltweite Übersäuerung der Böden, die Überdüngung sowie die Land- und Wassernutzung.« Poore, J.
+',
             },
             {
                 title: 'Ernährung',
                 url: '/ernährung',
                 image: '/undraw_Chef.svg',
-                intro: 'Als gesunde Ernährung wird eine Ernährung bezeichnet, die den Bedarf aller Nährstoffe deckt und dabei möglichst wenig Lebensmittel, mit potentiell abträglichen Effekten bei erhöhtem Verzehr, beinhaltet. Grundsätzlich kann jede Ernährungsform, ob vegan, vegetarisch, omnivor o.Ä. ungesund sein, wenn diese Faktoren nicht berücksichtigt werden.',
+                intro: 'Für unseren Nährstoffbedarf besteht absolut keine Notwendigkeit, tierische Produkte zu konsumieren. Des Weiteren zeigen Untersuchungen, dass die vegane Ernährungsweise im Vergleich zur Omnivoren Ernährung mit besseren gesundheitlichen Effekten einhergeht. Zum Beispiel mit einem geringeren Risiko für Herz-Kreislauf-Erkrankungen, für einige Krebsarten, Übergewicht, Hypertonie (Bluthochdruck), Typ-2 Diabetes und allgemein einer geringen Sterblichkeit.',
             },
             {
                 title: 'Ethik',
                 url: '/ethik',
                 image: '/undraw_wandering_mind.svg',
-                intro: 'Jeder Schritt in Richtung einer veganer Lebensweise ist ein guter Schritt. Hier werden viele Produktionsvorgänge unterschiedlicher tierischer Produkte geschildert. Grundsätzlich ist, aus ethischer Sicht, jede Verwendung von tierischen Produkten nicht vertretbar.',
+                intro: 'Unsere Gesellschaft und die Wissenschaft entwickeln sich stehts weiter, doch bei der ethischen Frage ob wir Lebewesen für unseren Genuss quälen und töten dürfen, scheint es nur ganz langsam Fortschritt zu geben. Wir haben täglich die Wahl: Kaufen wir tierische Produkte und verantworten das qualvolle Leben und den Tod von Lebewesen, die nicht sterben wollen, oder stehen wir für die Lebewesen ein, die selbst keine Stimme haben sich zu äußern und leben ohne Tierleid zu verursachen – vegan.
+',
             },
         ],
     }),
