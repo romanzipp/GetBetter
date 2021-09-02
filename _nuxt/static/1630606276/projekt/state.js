cf66736a59aca9485452122228995ac6ed4785df
aaamat <aaamat@users.noreply.github.com>
2021-09-02 18:11:18 +0000

    deploy: aac3a6ee0c3be9b0a0a3a4a0fc713364609197d9

---
+++ _nuxt/static/1630606276/projekt/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630606276",layout:"content",error:a,serverRendered:true,routePath:"\u002Fprojekt",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f20726f8"}}}}(null));
