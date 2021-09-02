ca1a4b2bdf652d2c2d73e2a54c2817c4e73bc3a4
aaamat <89915466+aaamat@users.noreply.github.com>
2021-09-02 16:48:38 +0200

    Update links.js

--- mixins/links.js
+++ mixins/links.js
@@ -5,8 +5,7 @@ export default {
                 title: 'Umwelt',
                 url: '/umwelt',
                 image: '/undraw_Nature_fun_re.svg',
-                intro: 'Unser Planet ist unser Lebensraum, für alle jetzigen und für alle kommenden Generationen. Unser oberstes Ziel sollte es sein, diesen Lebensraum zu bewahren. »Eine vegane Ernährung ist wahrscheinlich die effektivste Weise den eigenen ökologischen Fußabdruck zu reduzieren und das nicht nur in Bezug auf Treibhausgase sondern auch auf die weltweite Übersäuerung der Böden, die Überdüngung sowie die Land- und Wassernutzung.« Poore, J.
-',
+                intro: 'Unser Planet ist unser Lebensraum, für alle jetzigen und für alle kommenden Generationen. Unser oberstes Ziel sollte es sein, diesen Lebensraum zu bewahren. »Eine vegane Ernährung ist wahrscheinlich die effektivste Weise den eigenen ökologischen Fußabdruck zu reduzieren und das nicht nur in Bezug auf Treibhausgase sondern auch auf die weltweite Übersäuerung der Böden, die Überdüngung sowie die Land- und Wassernutzung.« Poore, J.',
             },
             {
                 title: 'Ernährung',
@@ -18,8 +17,7 @@ export default {
                 title: 'Ethik',
                 url: '/ethik',
                 image: '/undraw_wandering_mind.svg',
-                intro: 'Unsere Gesellschaft und die Wissenschaft entwickeln sich stehts weiter, doch bei der ethischen Frage ob wir Lebewesen für unseren Genuss quälen und töten dürfen, scheint es nur ganz langsam Fortschritt zu geben. Wir haben täglich die Wahl: Kaufen wir tierische Produkte und verantworten das qualvolle Leben und den Tod von Lebewesen, die nicht sterben wollen, oder stehen wir für die Lebewesen ein, die selbst keine Stimme haben sich zu äußern und leben ohne Tierleid zu verursachen – vegan.
-',
+                intro: 'Unsere Gesellschaft und die Wissenschaft entwickeln sich stehts weiter, doch bei der ethischen Frage ob wir Lebewesen für unseren Genuss quälen und töten dürfen, scheint es nur ganz langsam Fortschritt zu geben. Wir haben täglich die Wahl: Kaufen wir tierische Produkte und verantworten das qualvolle Leben und den Tod von Lebewesen, die nicht sterben wollen, oder stehen wir für die Lebewesen ein, die selbst keine Stimme haben sich zu äußern und leben ohne Tierleid zu verursachen – vegan.',
             },
         ],
     }),
