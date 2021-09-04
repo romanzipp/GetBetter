e57974c043974886ab05f17e42ef5a8669230150
aaamat <aaamat@users.noreply.github.com>
2021-09-04 19:20:31 +0000

    deploy: 4c5582f35e24d882d1bbbeaac04df0c9a18c2d93

--- _nuxt/static/1630626365/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630626365",layout:"default",error:a,state:{dark:false},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6d0509b9"}}}}(null,"\u002F"));
