ed01028b234f3ea1e166eb3c29711372bb5c22f9
aaamat <aaamat@users.noreply.github.com>
2021-09-05 13:45:57 +0000

    deploy: ac6c17d011cd491d1431f3450af126b33370c7e8

--- _nuxt/static/1630845545/ethik/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630845545",layout:"default",error:a,state:{dark:false},serverRendered:true,routePath:"\u002Fethik",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"57509a4b"}}}}(null));
---
+++ _nuxt/static/1630849554/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630849554",layout:"default",error:a,state:{dark:false},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"16ab9b3b"}}}}(null,"\u002F"));
