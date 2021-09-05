2f98e6fc8242236b9fcc54a7b0f5111905f50fe7
ibbim <ibbim@users.noreply.github.com>
2021-09-05 21:33:44 +0000

    deploy: c105fece41684a2f4cdda2d2fbee2466a97f19af

---
+++ _nuxt/static/1630877622/ernaehrung/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630877622",layout:"default",error:a,state:{dark:false},serverRendered:true,routePath:"\u002Fernaehrung",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"53c284dd"}}}}(null));
