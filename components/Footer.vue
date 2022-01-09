558442a0ca555d3191bfef475c5389550aa1d7f8
Robin Kehl <robin.kehl@singular-it.de>
2022-01-09 14:26:11 +0100

    adjust footer accessibility

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,24 +1,28 @@
 <template>
 
-    <div class="flex justify-center mt-64 space-x-6 py-16">
+    <footer class="flex justify-center mt-64 space-x-6 py-16">
 
-        <a v-for="link in links"
-           :key="link.title"
-           :href="link.url"
-           target="_blank"
-           class="flex items-center dark:text-greener-50"
-           rel="noopener">
+        <ul class="flex gap-8">
+            <li v-for="link in links"
+                :key="link.title">
+                <a :href="link.url"
+                   target="_blank"
+                   aria-label="link.title"
+                   class="flex items-center dark:text-greener-50"
+                   rel="noopener">
 
-            <img v-if="link.logo"
-                 :src="link.logo"
-                 :alt="link.title"
-                 class="h-5 mr-2">
+                    <img v-if="link.logo"
+                         :src="link.logo"
+                         :alt="link.title"
+                         class="h-5 mr-2">
 
-            {{ link.title }}
+                    {{ link.title }}
 
-        </a>
+                </a>
+            </li>
+        </ul>
 
-    </div>
+    </footer>
 
 </template>
 
