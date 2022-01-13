85f10701e4b49b25d5492743d1362b93ccd9ed77
romanzipp <romanzipp@users.noreply.github.com>
2022-01-13 16:57:35 +0000

    deploy: 6c787e53316f058f4afe858fcdcb9427b11e9da8

---
+++ _nuxt/static/1642093053/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642093053",layout:"default",error:a,state:{dark:false},serverRendered:true,routePath:b,config:{baseUrl:"https:\u002F\u002Fget-better.me",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"27537cf9"}}}}(null,"\u002F"));
