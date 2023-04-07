c472b1ddb08d33f524c5ae1bd0702a51cd932be5
aaamat <aaamat@users.noreply.github.com>
2023-04-07 10:27:07 +0000

    deploy: 9167dab8d2b3d12d64eda7b3a0b6901b91cd4856

---
+++ _nuxt/static/1680863225/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1680863225",layout:"default",error:a,state:{dark:false},serverRendered:true,routePath:b,config:{baseUrl:"https:\u002F\u002Fget-better.me",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"13f88101"}}}}(null,"\u002F"));
