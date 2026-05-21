88e8fe1f38a3e459e9b095f9efd85e500b9bd1a0
Roman Zipp <ich@ich.wtf>
2026-05-22 00:38:41 +0200

    Update footer links

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,17 +2,10 @@
 
     <div class="flex justify-center mt-64 space-x-6 py-16">
 
-        <a v-for="link in links"
-           :key="link.title"
-           :href="link.url"
-           target="_blank"
-           class="flex items-center dark:text-greener-50"
-           rel="noopener">
+        <a v-for="link in links" :key="link.title" :href="link.url" target="_blank"
+            class="flex items-center dark:text-greener-50" rel="noopener">
 
-            <img v-if="link.logo"
-                 :src="link.logo"
-                 :alt="link.title"
-                 class="h-5 mr-2">
+            <img v-if="link.logo" :src="link.logo" :alt="link.title" class="h-5 mr-2">
 
             {{ link.title }}
 
@@ -34,9 +27,9 @@ export default {
 
     data: () => ({
         links: [
-            { title: 'Open Source', url: 'https://github.com/romanzipp/GetBetter', logo: GitHubLogo },
-            { title: 'Impressum', url: 'https://ich.wtf/impressum' },
-            { title: 'Datenschutz', url: 'https://ich.wtf/datenschutzerklaerung' },
+            { title: 'Open Source', url: 'https://codeberg.org/romanzipp/get-better.me', logo: GitHubLogo },
+            { title: 'Impressum', url: 'https://romanzipp.com/impressum' },
+            { title: 'Datenschutz', url: 'https://romanzipp.com/datenschutzerklaerung' },
         ],
     }),
 };
